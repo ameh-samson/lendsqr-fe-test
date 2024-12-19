6156519dcaf120a36b4ccb5aab72aab7be501d57
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-12-19 16:03:41 +0100

    add user details component

--- src/screens/userDetails/UserDetails.tsx
+++ src/screens/userDetails/UserDetails.tsx
@@ -1,5 +1,25 @@
+import styles from "./UserDetails.module.scss";
+import backIcon from "@/assets/svg/back-icon.svg";
+import { Link } from "react-router-dom";
+
 const UserDetails = () => {
-  return <div>UserDetails</div>;
+  return (
+    <section className={styles.userdetails}>
+      <Link to="/users" className={styles.backButton}>
+        <img src={backIcon} alt="back" />
+        <span>Back to Users</span>
+      </Link>
+
+      <div className={styles.userdetailsHeader}>
+        <h1>User Details</h1>
+
+        <div>
+          <button>Blacklist User</button>
+          <button>Activate User</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default UserDetails;
