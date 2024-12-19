74405dd2e95526fc1c2718d2393d593c5f064447
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-12-19 16:15:14 +0100

    remove dashboard and home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/screens/home/Home";
-import Dashboard from "@/screens/dashboard/Dashboard";
+
 import Users from "@/screens/users/Users";
 import UserDetails from "@/screens/userDetails/UserDetails";
 
 const RouteConfig = () => {
   return (
     <Routes>
-      <Route path="/" index element={<Home />} />
-      <Route path="/dashboard" index element={<Dashboard />} />
       <Route path="/users" index element={<Users />} />
       <Route path="/user/:id" index element={<UserDetails />} />
     </Routes>
