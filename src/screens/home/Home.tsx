d761b9b30dccdb5bcdfa6447906344c65ff1bda9
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-12-20 13:54:39 +0100

    add media query for login page

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,25 +2,43 @@ import LoginForm from "@/components/home/loginForm/LoginForm";
 import loginImage from "@/assets/png/pablo-sign-in.png";
 import logo from "@/assets/svg/logo.svg";
 import styles from "./Home.module.scss";
+import Loader from "@/components/loader/Loader";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <section className={styles.home}>
-      <div className={styles.container}>
-        <div className={styles.leftContainer}>
-          <img src={logo} alt="logo" className={styles.logo} />
-          <img src={loginImage} alt="login" />
-        </div>
+  const [loading, setLoading] = useState(true);
 
-        <div className={styles.rightContainer}>
-          <h1>Welcome!</h1>
-          <p>Enter details to login.</p>
-          <div className={styles.formContainer}>
-            <LoginForm />
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className={styles.home}>
+          <div className={styles.container}>
+            <div className={styles.leftContainer}>
+              <img src={logo} alt="logo" className={styles.logo} />
+              <img src={loginImage} alt="login" className={styles.loginImage} />
+            </div>
+
+            <div className={styles.rightContainer}>
+              <h1>Welcome!</h1>
+              <p>Enter details to login.</p>
+              <div className={styles.formContainer}>
+                <LoginForm />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
 
